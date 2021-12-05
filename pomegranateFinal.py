f41e1092795085cd152442b2516d170b867c3e3d
SquishyWizard <89790693+SquishyWizard@users.noreply.github.com>
2021-12-05 18:07:37 -0500

    Add files via upload

--- pomegranateFinal.py
+++ pomegranateFinal.py
@@ -8,7 +8,7 @@ deck = ["AH","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH",
         "AD","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD",
         "AC","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC"]
 graveyard = []
-# pot = 0
+pot = 0
 # current_bet = 0
 dealers_hand = deck
 
@@ -59,11 +59,32 @@ def callRaiseFold(self, pot, current_bet):
         
     if action == "3":
         print("You Lose!")
+        #gameState = False
         #discard player's hand into graveyard
         
     # -- TO PRINT GRAVEYARD --
     # for card in graveyard:
     #     print(card, end = " ")
+
+
+def turn():
+    choice = (input("Swap Cards? (1 for yes, 2 for no)"))
+    if choice == 1:
+        pass
+        #calls method to replace cards
+        #cs.draw(player = 0, deck = 0, cards = 0))
+        #callRaiseFold(pot = 0, current_bet = 0)
+    else:
+        pass
+        #callRaiseFold(pot = 0, current_bet= 0)
+ 
+    #call scoring to find the final score
+
+
+#MIAN FUNCTION
+#loop to stop the turns when game is over
+# while gameState = True:
+#     turn()
 
 
 print("\nLet's Play\n")
@@ -80,21 +101,7 @@ print("\n")
 #takes player's cards out of the dealer's hand
 dealers_hand = dealers_hand[5:]
 
-def turn():
-    choice = (input("Swap Cards? (1 for yes, 2 for no)"))
-    if choice == 1:
-        pass
-        #calls method to replace cards
-        #cs.draw(player = 0, deck = 0, cards = 0))
-        #callRaiseFold(pot = 0, current_bet = 0)
-    else:
-        pass
-        #callRaiseFold(pot = 0, current_bet= 0)
- 
-    #call scoring to find the final score
-
-
-#main function
-#loop to stop the turns when game is over
-# while gameState = True:
-#     turn()
+# goodbye
+# OR
+# ask to play again
+# recursive to beginning

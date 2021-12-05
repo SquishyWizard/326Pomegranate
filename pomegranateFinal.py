ad154780c2c9b9ec38866169e4e8490f90a990c0
SquishyWizard <89790693+SquishyWizard@users.noreply.github.com>
2021-12-05 17:48:05 -0500

    Add files via upload

--- pomegranateFinal.py
+++ pomegranateFinal.py
@@ -1,9 +1,8 @@
 import random
-import time
 import Scoring as sc
 import cardswap as cs
 
-#Creates and shuffles deck, 
+#Creates and shuffles deck
 deck = ["AH","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH",
         "AS","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS",
         "AD","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD",
@@ -18,17 +17,21 @@ class Player():
     def __init__(self, name, hand):
         self.name = name
         self.hand = hand
+  
         
 #current_bet = int(input("Enter Starting Bet: "))
     #raise value errror if negative or not a number
 # pot += int(current_bet)
 
 
+# -- TO PRINT OTHER HAND --
 # print("\nOpponenet's Hand:")
 # opponent_hand = dealers_hand[:5]
 # for card in opponent_hand:
 #     print(card, end = " ")
-    
+
+
+# -- TO PRINT  REMAINING CARDS --
 # dealers_hand = dealers_hand[5:]
 # print("\nDealer's Hand:")
 # for card in dealers_hand:
@@ -49,18 +52,18 @@ def callRaiseFold(pot, current_bet):
             callRaiseFold(pot,current_bet)
         else:
             current_bet = int(input("Enter Raised Bet: "))
-            #raise value errror if bet is lower than the best before
-            #raise value error if not a number
+            #raise value errror if
+            #   bet is lower than before
+            #   not a number
             pot += current_bet
         
     if action == "3":
         print("You Lose!")
-    
+        #discard player's hand into graveyard
+        
+    # -- TO PRINT GRAVEYARD --
     # for card in graveyard:
     #     print(card, end = " ")
-          
-
-    #discard player's hand into graveyard
 
 
 print("\nLet's Play\n")
